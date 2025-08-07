4306a199104f73aeda0f6f173cdd729a05c0c8c8
Datra ROG <fattahindralesmana@yahoo.com>
2025-08-07 14:07:44 +0700

    fix: correct syntax in clientlayout component

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,18 +1,17 @@
-// Lokasi File: src/app/ClientLayout.tsx
+// File: src/app/ClientLayout.tsx
 'use client';
 import React from 'react';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  // Ini adalah style "pemaksa" kita
   const forceSolidStyle = {
-        '--tw-bg-opacity': '1',
-        '--tw-text-opacity': '1',
-        '--tw-border-opacity': '1',
-      };
+    '--tw-bg-opacity': '1',
+    '--tw-text-opacity': '1',
+    '--tw-border-opacity': '1',
+  };
 
-      return (
-        // Kita tambahkan "as React.CSSProperties" di sini
-        <div style={forceSolidStyle as React.CSSProperties}>
-          {children}
-        </div>
-      );
+  return (
+    <div style={forceSolidStyle as React.CSSProperties}>
+      {children}
+    </div>
+  );
+}
